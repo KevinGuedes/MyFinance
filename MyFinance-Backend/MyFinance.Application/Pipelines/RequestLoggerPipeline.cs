28b0d82a568047ee32f8002ba7c359f9654ccc64
KevinGuedes <kevinguedes@edu.unifor.br>
2022-06-22 17:40:15 -0300

    feat(backend): fail fast, unit of work and request logger pipelines

---
+++ MyFinance-Backend/MyFinance.Application/Pipelines/RequestLoggerPipeline.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace MyFinance.Application.Pipelines
+{
+    public class RequestLoggerPipeline<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<RequestLoggerPipeline<TRequest, TResponse>> _logger;
+
+        public RequestLoggerPipeline(ILogger<RequestLoggerPipeline<TRequest, TResponse>> logger)
+            => _logger = logger;
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var requestName = request.GetType().Name;
+            _logger.LogInformation("Handling {RequestName}", requestName);
+            var result = await next();
+            _logger.LogInformation("{RequestName} successfully handled", requestName);
+            return result;
+        }
+    }
+}
