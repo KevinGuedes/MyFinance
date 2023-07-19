a69a71180286d8fefd349cc164085568295c4beb
KevinGuedes <kevinguedes1@gmail.com>
2023-07-19 20:59:55 -0300

    feat: validation for name when updating bu

--- MyFinance-Backend/MyFinance.Application/BusinessUnits/Commands/UpdateBusinessUnit/UpdateBusinessUnitValidator.cs
+++ MyFinance-Backend/MyFinance.Application/BusinessUnits/Commands/UpdateBusinessUnit/UpdateBusinessUnitValidator.cs
@@ -1,30 +1,35 @@
 ﻿using FluentValidation;
 using MyFinance.Domain.Interfaces;
 
-namespace MyFinance.Application.BusinessUnits.Commands.UpdateBusinessUnit
+namespace MyFinance.Application.BusinessUnits.Commands.UpdateBusinessUnit;
+
+public sealed class UpdateBusinessUnitValidator : AbstractValidator<UpdateBusinessUnitCommand>
 {
-    public sealed class UpdateBusinessUnitValidator : AbstractValidator<UpdateBusinessUnitCommand>
+    private readonly IBusinessUnitRepository _businessUnitRepository;
+
+    public UpdateBusinessUnitValidator(IBusinessUnitRepository businessUnitRepository)
     {
-        private readonly IBusinessUnitRepository _businessUnitRepository;
+        _businessUnitRepository = businessUnitRepository;
+        ClassLevelCascadeMode = CascadeMode.Stop;
 
-        public UpdateBusinessUnitValidator(IBusinessUnitRepository businessUnitRepository)
-        {
-            _businessUnitRepository = businessUnitRepository;
-            ClassLevelCascadeMode = CascadeMode.Stop;
+        RuleFor(command => command.Name)
+           .Cascade(CascadeMode.Stop)
+           .NotNull().WithMessage("{PropertyName} must not be null")
+           .NotEmpty().WithMessage("{PropertyName} must not be empty")
+           .Length(3, 50).WithMessage("{PropertyName} must have between 3 and 50 characters")
+           .MustAsync(async (businessUnitName, cancellationToken) =>
+           {
+               var exists = await _businessUnitRepository.ExistsByNameAsync(businessUnitName, cancellationToken);
+               return !exists;
+           }).WithMessage("This {PropertyName} has already been taken");
 
-            RuleFor(command => command.Name)
-                .NotNull().WithMessage("{PropertyName} must not be null")
-                .NotEmpty().WithMessage("{PropertyName} must not be empty")
-                .Length(3, 50).WithMessage("{PropertyName} must have between 3 and 50 characters");
-
-            RuleFor(command => command.BusinessUnitId)
-                .Cascade(CascadeMode.Stop)
-                .NotEqual(Guid.Empty).WithMessage("{PropertyName} invalid")
-                .MustAsync(async (businessUnitId, cancellationToken) =>
-                {
-                    var exists = await _businessUnitRepository.ExistsByIdAsync(businessUnitId, cancellationToken);
-                    return exists;
-                }).WithMessage("Business Unit not found");
-        }
+        RuleFor(command => command.BusinessUnitId)
+            .Cascade(CascadeMode.Stop)
+            .NotEqual(Guid.Empty).WithMessage("{PropertyName} invalid")
+            .MustAsync(async (businessUnitId, cancellationToken) =>
+            {
+                var exists = await _businessUnitRepository.ExistsByIdAsync(businessUnitId, cancellationToken);
+                return exists;
+            }).WithMessage("Business Unit not found");
     }
 }
