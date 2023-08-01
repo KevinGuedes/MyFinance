869916185eabeccfd260d35b2d6e0e2173ffc8b2
KevinGuedes <kevinguedes1@gmail.com>
2023-07-31 21:16:12 -0300

    fix: fixes after exploratory tests

--- MyFinance-Backend/MyFinance.Application/UseCases/MonthlyBalances/DTOs/MonthlyBalanceDTO.cs
+++ MyFinance-Backend/MyFinance.Application/UseCases/MonthlyBalances/DTOs/MonthlyBalanceDTO.cs
@@ -1,4 +1,5 @@
 ﻿using MyFinance.Application.Common.DTO;
+using MyFinance.Application.UseCases.Transfers.DTOs;
 
 namespace MyFinance.Application.UseCases.MonthlyBalances.DTOs;
 
@@ -6,21 +7,22 @@ public sealed class MonthlyBalanceDTO : BaseDTO
 {
     public double Income { get; set; }
     public double Outcome { get; set; }
+    public double Balance { get; set; }
     public int ReferenceMonth { get; set; }
     public int ReferenceYear { get; set; }
     public Guid BusinessUnitId { get; set; }
 
     public MonthlyBalanceDTO(
-        Guid id,
         double income,
         double outcome,
+        double balance,
         int referenceMonth,
         int referenceYear,
         Guid businessUnitId)
     {
-        Id = id;
         Income = income;
         Outcome = outcome;
+        Balance = balance;
         ReferenceMonth = referenceMonth;
         ReferenceYear = referenceYear;
         BusinessUnitId = businessUnitId;
