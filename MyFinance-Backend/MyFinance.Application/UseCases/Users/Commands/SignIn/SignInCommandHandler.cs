d351e9cce0c63e438f6da6281379f5d758d8e488
Kevin Guedes <kevinguedes1@gmail.com>
2024-02-11 20:56:47 -0300

    fix: proper flow for sign in

--- MyFinance-Backend/MyFinance.Application/UseCases/Users/Commands/SignIn/SignInCommandHandler.cs
+++ MyFinance-Backend/MyFinance.Application/UseCases/Users/Commands/SignIn/SignInCommandHandler.cs
@@ -9,8 +9,8 @@ using MyFinance.Infra.Services.PasswordHasher;
 namespace MyFinance.Application.UseCases.Users.Commands.SignIn;
 
 public sealed class SignInCommandHandler(
-    ILogger<SignInCommandHandler> logger, 
-    IUserRepository userRepository, 
+    ILogger<SignInCommandHandler> logger,
+    IUserRepository userRepository,
     IAuthService authService,
     IPasswordHasher passwordHasher) : ICommandHandler<SignInCommand>
 {
@@ -22,27 +22,22 @@ public sealed class SignInCommandHandler(
     public async Task<Result> Handle(SignInCommand request, CancellationToken cancellationToken)
     {
         var user = await _userRepository.GetByEmailAsync(request.Email, cancellationToken);
-        if (user is null)
-        {
-            _logger.LogWarning("User not registered");
-            var errorMessage = string.Format("User not registered");
-            var entityNotFoundError = new EntityNotFoundError(errorMessage);
-            return Result.Fail(entityNotFoundError);
-        }
+        if (user is null) return HandleInvalidCredentials();
 
         var isPasswordValid = _passwordHasher.VerifyPassword(request.PlainTextPassword, user.PasswordHash);
-        if (!isPasswordValid)
-        {
-            _logger.LogWarning("Invalid credentials");
-            var errorMessage = string.Format("Invalid credentials");
-            var unauthorizedError = new UnauthorizedError(errorMessage);
-            return Result.Fail(unauthorizedError);
-        }
+        if (!isPasswordValid) return HandleInvalidCredentials();
 
         _logger.LogInformation("Signing in user");
         await _authService.SignInAsync(user.Email);
         _logger.LogInformation("User successfully signed in");
 
         return Result.Ok();
+    }
+
+    private Result HandleInvalidCredentials()
+    {
+        _logger.LogInformation("Invalid credentials");
+        var unauthorizedError = new UnauthorizedError("Invalid email or password");
+        return Result.Fail(unauthorizedError);
     }
 }
