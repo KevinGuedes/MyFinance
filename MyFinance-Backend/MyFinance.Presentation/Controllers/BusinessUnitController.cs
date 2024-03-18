d78e149c0b92e4a5f6014566014415f331b4b996
Kevin Guedes <kevinguedes1@gmail.com>
2024-03-18 12:03:55 -0300

    refactor: transfer and bu controllers

--- MyFinance-Backend/MyFinance.Presentation/Controllers/BusinessUnitController.cs
+++ MyFinance-Backend/MyFinance.Presentation/Controllers/BusinessUnitController.cs
@@ -1,54 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using MyFinance.Application.Abstractions.ApiServices;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using MyFinance.Application.Common.ApiResponses;
-using MyFinance.Application.UseCases.BusinessUnits.Commands.ArchiveBusinessUnit;
-using MyFinance.Application.UseCases.BusinessUnits.Commands.CreateBusinessUnit;
-using MyFinance.Application.UseCases.BusinessUnits.Commands.UpdateBusinessUnit;
-using MyFinance.Application.UseCases.BusinessUnits.DTOs;
+using MyFinance.Application.Mappers;
+using MyFinance.Application.UseCases.BusinessUnits.Commands.UnarchiveBusinessUnit;
+using MyFinance.Application.UseCases.BusinessUnits.Queries.GetBusinessUnits;
+using MyFinance.Contracts.BusinessUnit.Requests;
+using MyFinance.Contracts.BusinessUnit.Responses;
+using MyFinance.Contracts.Common;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace MyFinance.Presentation.Controllers;
 
 [SwaggerTag("Create, read and update Business Units")]
-public class BusinessUnitController(IBusinessUnitService businessUnitApiService) : ApiController
+public class BusinessUnitController(IMediator mediator) : ApiController(mediator)
 {
-    private readonly IBusinessUnitService _businessUnitApiService = businessUnitApiService;
-
     [HttpPost]
     [SwaggerOperation(Summary = "Creates a new Business Unit")]
-    [SwaggerResponse(StatusCodes.Status201Created, "Created Business Unit", typeof(BusinessUnitDTO))]
+    [SwaggerResponse(StatusCodes.Status201Created, "Created Business Unit", typeof(BusinessUnitResponse))]
     [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid payload", typeof(BadRequestResponse))]
     public async Task<IActionResult> CreateBusinessUnitAsync(
         [FromBody] [SwaggerRequestBody("Business unit payload", Required = true)]
-        CreateBusinessUnitCommand command,
+        CreateBusinessUnitRequest request,
         CancellationToken cancellationToken)
-        => ProcessResult(await _businessUnitApiService.CreateBusinessUnitAsync(command, cancellationToken), true);
+        => ProcessResult(await _mediator.Send(BusinessUnitMapper.RTC.Map(request), cancellationToken), true);
 
     [HttpGet]
     [SwaggerOperation(Summary = "Lists all Business Units with pagination")]
     [SwaggerResponse(StatusCodes.Status200OK, "List of all existing Business Units with pagination",
-        typeof(IEnumerable<BusinessUnitDTO>))]
+        typeof(PaginatedResponse<BusinessUnitResponse>))]
     [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid query parameters", typeof(BadRequestResponse))]
     public async Task<IActionResult> GetBusinessUnitsAsync(
         [FromQuery] [SwaggerParameter("Page number", Required = true)]
-        int page,
+        int pageNumber,
         [FromQuery] [SwaggerParameter("Units per page", Required = true)]
         int pageSize,
         CancellationToken cancellationToken)
-        => ProcessResult(await _businessUnitApiService.GetBusinessUnitsAsync(page, pageSize, cancellationToken));
+        => ProcessResult(await _mediator.Send(new GetBusinessUnitsQuery(pageNumber, pageSize), cancellationToken));
 
     [HttpPut]
     [SwaggerOperation(Summary = "Updates an existing Business Unit")]
-    [SwaggerResponse(StatusCodes.Status200OK, "Updated Business Unit", typeof(BusinessUnitDTO))]
+    [SwaggerResponse(StatusCodes.Status200OK, "Updated Business Unit", typeof(BusinessUnitResponse))]
     [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid payload", typeof(BadRequestResponse))]
     [SwaggerResponse(StatusCodes.Status404NotFound, "Business Unit not found", typeof(EntityNotFoundResponse))]
     [SwaggerResponse(StatusCodes.Status409Conflict, "The Business Unit was updated after the request was sent",
         typeof(UnprocessableEntityResponse))]
     public async Task<IActionResult> UpdateBusinessUnitAsync(
         [FromBody] [SwaggerRequestBody("Business Unit payload", Required = true)]
-        UpdateBusinessUnitCommand command,
+        UpdateBusinessUnitRequest request,
         CancellationToken cancellationToken)
-        => ProcessResult(await _businessUnitApiService.UpdateBusinessUnitAsync(command, cancellationToken));
+        => ProcessResult(await _mediator.Send(BusinessUnitMapper.RTC.Map(request), cancellationToken));
 
     [HttpPatch("{id:guid}")]
     [SwaggerOperation(Summary = "Unarchives an existing Business Unit")]
@@ -61,7 +61,7 @@ public class BusinessUnitController(IBusinessUnitService businessUnitApiService)
         [FromRoute] [SwaggerParameter("Id of the Business Unit to unarchive", Required = true)]
         Guid id,
         CancellationToken cancellationToken)
-        => ProcessResult(await _businessUnitApiService.UnarchiveBusinessUnitAsync(id, cancellationToken));
+        => ProcessResult(await _mediator.Send(new UnarchiveBusinessUnitCommand(id), cancellationToken));
 
     [HttpDelete]
     [SwaggerOperation(Summary = "Logically deletes (archives) an existing Business Unit")]
@@ -72,7 +72,7 @@ public class BusinessUnitController(IBusinessUnitService businessUnitApiService)
         typeof(UnprocessableEntityResponse))]
     public async Task<IActionResult> ArchiveBusinessUnitAsync(
         [FromBody] [SwaggerRequestBody("Payload to archvie a Business Unit", Required = true)]
-        ArchiveBusinessUnitCommand command,
+        ArchiveBusinessUnitRequest request,
         CancellationToken cancellationToken)
-        => ProcessResult(await _businessUnitApiService.ArchiveBusinessUnitAsync(command, cancellationToken));
+        => ProcessResult(await _mediator.Send(BusinessUnitMapper.RTC.Map(request), cancellationToken));
 }
--- MyFinance-Backend/MyFinance.Presentation/Controllers/TransferController.cs
+++ MyFinance-Backend/MyFinance.Presentation/Controllers/TransferController.cs
@@ -1,41 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using MyFinance.Application.Abstractions.ApiServices;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using MyFinance.Application.Common.ApiResponses;
-using MyFinance.Application.UseCases.Transfers.Commands.RegisterTransfer;
-using MyFinance.Application.UseCases.Transfers.Commands.UpdateTransfer;
-using MyFinance.Application.UseCases.Transfers.DTOs;
+using MyFinance.Application.Mappers;
+using MyFinance.Application.UseCases.Transfers.Commands.DeleteTransfer;
+using MyFinance.Contracts.Transfer.Requests;
+using MyFinance.Contracts.Transfer.Responses;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace MyFinance.Presentation.Controllers;
 
 [SwaggerTag("Create, update and delete Transfers")]
-public class TransferController(ITransferService transferApiService) : ApiController
+public class TransferController(IMediator mediator) : ApiController(mediator)
 {
-    private readonly ITransferService _transferApiService = transferApiService;
-
     [HttpPost]
     [SwaggerOperation(Summary = "Registers a new Transfer")]
-    [SwaggerResponse(StatusCodes.Status201Created, "Transfer registered", typeof(TransferDTO))]
+    [SwaggerResponse(StatusCodes.Status201Created, "Transfer registered", typeof(TransferResponse))]
     [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid payload", typeof(BadRequestResponse))]
     [SwaggerResponse(StatusCodes.Status404NotFound, "Business Unit not found", typeof(EntityNotFoundResponse))]
     public async Task<IActionResult> RegisterTransfersAsync(
         [FromBody] [SwaggerRequestBody("Transfers' payload", Required = true)]
-        RegisterTransferCommand command,
+        RegisterTransferRequest request,
         CancellationToken cancellationToken)
-        => ProcessResult(await _transferApiService.RegisterTransferAsync(command, cancellationToken), true);
+        => ProcessResult(await _mediator.Send(TransferMapper.RTC.Map(request), cancellationToken), true);
 
     [HttpPut]
     [SwaggerOperation(Summary = "Updates an existing Transfer")]
-    [SwaggerResponse(StatusCodes.Status200OK, "Updated Transfer", typeof(TransferDTO))]
+    [SwaggerResponse(StatusCodes.Status200OK, "Updated Transfer", typeof(TransferResponse))]
     [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid payload", typeof(BadRequestResponse))]
     [SwaggerResponse(StatusCodes.Status404NotFound, "Transfer not found", typeof(EntityNotFoundResponse))]
     [SwaggerResponse(StatusCodes.Status409Conflict, "The Transfer was updated after the request was sent",
         typeof(UnprocessableEntityResponse))]
     public async Task<IActionResult> UpdateTransferAsync(
         [FromBody] [SwaggerRequestBody("Transfers' payload", Required = true)]
-        UpdateTransferCommand command,
+        UpdateTransferRequest request,
         CancellationToken cancellationToken)
-        => ProcessResult(await _transferApiService.UpdateTransferAsync(command, cancellationToken));
+        => ProcessResult(await _mediator.Send(TransferMapper.RTC.Map(request), cancellationToken));
 
     [HttpDelete("{id:guid}")]
     [SwaggerOperation(Summary = "Deletes an existing Transfer")]
@@ -48,5 +47,5 @@ public class TransferController(ITransferService transferApiService) : ApiContro
         [FromRoute] [SwaggerParameter("Transfer Id", Required = true)]
         Guid id,
         CancellationToken cancellationToken)
-        => ProcessResult(await _transferApiService.DeleteTransferAsync(id, cancellationToken));
+        => ProcessResult(await _mediator.Send(new DeleteTransferCommand(id), cancellationToken));
 }
