ec6ca7ca42e64e3ee52adf0819e6adebb03e9834
Kevin Guedes <kevinguedes1@gmail.com>
2024-03-28 12:34:58 -0300

    refactor: removing generic approach for entity not found error

--- MyFinance-Backend/MyFinance.Application/Common/Errors/EntityNotFoundError.cs
+++ MyFinance-Backend/MyFinance.Application/Common/Errors/EntityNotFoundError.cs
@@ -5,9 +5,3 @@ namespace MyFinance.Application.Common.Errors;
 public class EntityNotFoundError(string message) : BaseError(message)
 {
 }
-
-public class EntityNotFoundError<TEntity>(Guid id) 
-    : BaseError($"{nameof(TEntity)} with Id {id} not found")
-    where TEntity : Entity
-{
-}
