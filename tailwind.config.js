8615c083a8b98a90c81309bdabe920d1fc790e9e
Kevin Guedes <kevinguedes1@gmail.com>
2024-05-09 13:16:35 -0300

    fix: removing files from wrogn folder

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-import defaultTheme from 'tailwindcss/defaultTheme'
-
-export const content = ['./index.html', './src/**/*.{js,ts,jsx,tsx}']
-export const plugins = []
-export const theme = {}
