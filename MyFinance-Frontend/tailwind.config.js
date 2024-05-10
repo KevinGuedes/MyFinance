ae0cf778a2f596dcde60d9029938a366e53cfe3d
Kevin Guedes <kevinguedes1@gmail.com>
2024-05-10 08:15:45 -0300

    feat: shadcn setup

--- MyFinance-Frontend/tailwind.config.js
+++ MyFinance-Frontend/tailwind.config.js
@@ -1,13 +1,37 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from 'tailwindcss/defaultTheme'
 
-export const plugins = []
-export const content = ['./index.html', './src/**/*.{js,ts,jsx,tsx}']
+export const darkMode = ['class']
+export const plugins = [require('tailwindcss-animate')]
+export const prefix = ''
+export const content = [
+  './pages/**/*.{ts,tsx}',
+  './components/**/*.{ts,tsx}',
+  './app/**/*.{ts,tsx}',
+  './src/**/*.{ts,tsx}',
+]
+
 export const theme = {
+  container: {
+    center: true,
+    padding: '2rem',
+    screens: {
+      '2xl': '1400px',
+    },
+  },
   extend: {
-    fontFamily: {
-      serif: ['"Playfair Display"', ...defaultTheme.fontFamily.serif],
-      sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans],
+    keyframes: {
+      'accordion-down': {
+        from: { height: '0' },
+        to: { height: 'var(--radix-accordion-content-height)' },
+      },
+      'accordion-up': {
+        from: { height: 'var(--radix-accordion-content-height)' },
+        to: { height: '0' },
+      },
+    },
+    animation: {
+      'accordion-down': 'accordion-down 0.2s ease-out',
+      'accordion-up': 'accordion-up 0.2s ease-out',
     },
   },
 }
