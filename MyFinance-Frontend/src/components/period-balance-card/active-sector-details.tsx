c6ee9984c32b8bf5682c63a163b2ffc9fccf35e2
Kevin Guedes <kevinguedes1@gmail.com>
2024-05-20 13:30:35 -0300

    refactor: better pie chart

---
+++ MyFinance-Frontend/src/components/period-balance-card/active-sector-details.tsx
@@ -0,0 +1,92 @@
+import { Sector } from 'recharts'
+import { PieSectorDataItem } from 'recharts/types/polar/Pie'
+
+import { toMoney } from '@/lib/utils'
+
+interface ActiveSectorDetailsProps extends PieSectorDataItem {}
+
+export function ActiveSectorDetails({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  startAngle,
+  endAngle,
+  fill,
+  name,
+  percent,
+  value,
+}: ActiveSectorDetailsProps) {
+  const RADIAN = Math.PI / 180
+
+  const sin = Math.sin(-RADIAN * midAngle!)
+  const cos = Math.cos(-RADIAN * midAngle!)
+  const sx = cx! + (outerRadius! + 10) * cos
+  const sy = cy! + (outerRadius! + 10) * sin
+  const mx = cx! + (outerRadius! + 30) * cos
+  const my = cy! + (outerRadius! + 30) * sin
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22
+  const ey = my
+  const textAnchor = cos >= 0 ? 'start' : 'end'
+
+  return (
+    <g>
+      <text
+        x={cx}
+        y={cy}
+        dy={8}
+        textAnchor="middle"
+        fill={fill}
+        className="font-bold"
+      >
+        {name}
+      </text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+        stroke="white"
+        strokeWidth={2}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius! + 6}
+        outerRadius={outerRadius! + 10}
+        fill={fill}
+      />
+      <path
+        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+        stroke={fill}
+        fill="none"
+      />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        textAnchor={textAnchor}
+        fill={fill}
+        className="font-bold"
+      >
+        {toMoney(value!)}
+      </text>
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        dy={18}
+        textAnchor={textAnchor}
+        fill={fill}
+        className="text-xs font-bold"
+      >
+        {`${(percent! * 100).toFixed(2)}%`}
+      </text>
+    </g>
+  )
+}
--- MyFinance-Frontend/src/components/period-balance-card/period-balance-chart.tsx
+++ MyFinance-Frontend/src/components/period-balance-card/period-balance-chart.tsx
@@ -1,112 +1,24 @@
 import { useState } from 'react'
-import {
-  Cell,
-  Legend,
-  Pie,
-  PieChart,
-  ResponsiveContainer,
-  Sector,
-} from 'recharts'
-import { PieSectorDataItem } from 'recharts/types/polar/Pie'
+import { Cell, Legend, Pie, PieChart, ResponsiveContainer } from 'recharts'
 
-import { toMoney } from '@/lib/utils'
+import { ActiveSectorDetails } from './active-sector-details'
 
 const data = [
-  { name: 'Income', value: 400 },
-  { name: 'Balance', value: 300 },
-  { name: 'Outcome', value: 300 },
+  { name: 'Balance', value: 322 },
+  { name: 'Income', value: 440 },
+  { name: 'Outcome', value: 673 },
 ]
-const COLORS = ['#2563eb', '#64748b', '#7f1d1d']
+const COLORS = ['#64748b', '#2563eb', '#7f1d1d']
 
 export function PeriodBalanceChart() {
   const [activeIndex, setActiveIndex] = useState<number | undefined>(undefined)
 
-  const renderActiveShape = (pieSectorDataItem: PieSectorDataItem) => {
-    const RADIAN = Math.PI / 180
+  function resetActiveIndex() {
+    setActiveIndex(undefined)
+  }
 
-    const {
-      cx,
-      cy,
-      midAngle,
-      innerRadius,
-      outerRadius,
-      startAngle,
-      endAngle,
-      fill,
-      name,
-      percent,
-      value,
-    } = pieSectorDataItem
-
-    const sin = Math.sin(-RADIAN * midAngle!)
-    const cos = Math.cos(-RADIAN * midAngle!)
-    const sx = cx! + (outerRadius! + 10) * cos
-    const sy = cy! + (outerRadius! + 10) * sin
-    const mx = cx! + (outerRadius! + 30) * cos
-    const my = cy! + (outerRadius! + 30) * sin
-    const ex = mx + (cos >= 0 ? 1 : -1) * 22
-    const ey = my
-    const textAnchor = cos >= 0 ? 'start' : 'end'
-
-    return (
-      <g>
-        <text
-          x={cx}
-          y={cy}
-          dy={8}
-          textAnchor="middle"
-          fill={fill}
-          className="font-bold"
-        >
-          {name}
-        </text>
-        <Sector
-          cx={cx}
-          cy={cy}
-          innerRadius={innerRadius}
-          outerRadius={outerRadius}
-          startAngle={startAngle}
-          endAngle={endAngle}
-          fill={fill}
-          stroke="white"
-          strokeWidth={2}
-        />
-        <Sector
-          cx={cx}
-          cy={cy}
-          startAngle={startAngle}
-          endAngle={endAngle}
-          innerRadius={outerRadius! + 6}
-          outerRadius={outerRadius! + 10}
-          fill={fill}
-        />
-        <path
-          d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
-          stroke={fill}
-          fill="none"
-        />
-        <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-        <text
-          x={ex + (cos >= 0 ? 1 : -1) * 12}
-          y={ey}
-          textAnchor={textAnchor}
-          fill={fill}
-          className="font-bold"
-        >
-          {toMoney(value!)}
-        </text>
-        <text
-          x={ex + (cos >= 0 ? 1 : -1) * 12}
-          y={ey}
-          dy={18}
-          textAnchor={textAnchor}
-          fill={fill}
-          className="text-xs font-bold"
-        >
-          {`${(percent! * 100).toFixed(2)}%`}
-        </text>
-      </g>
-    )
+  function showSectorDetailsFor(index: number) {
+    setActiveIndex(index)
   }
 
   return (
@@ -114,18 +26,18 @@ export function PeriodBalanceChart() {
       <ResponsiveContainer width="100%" height="100%">
         <PieChart width={400} height={400}>
           <Pie
-            onMouseLeave={() => setActiveIndex(undefined)}
-            onMouseEnter={(_, index) => setActiveIndex(index)}
             data={data}
+            dataKey="value"
             legendType="rect"
             cx="50%"
             cy="50%"
-            activeIndex={activeIndex}
-            activeShape={renderActiveShape}
             outerRadius={80}
             innerRadius={60}
-            dataKey="value"
-            paddingAngle={3}
+            paddingAngle={5}
+            activeIndex={activeIndex}
+            activeShape={<ActiveSectorDetails />}
+            onMouseLeave={resetActiveIndex}
+            onMouseEnter={(_, index) => showSectorDetailsFor(index)}
           >
             {data.map((_, index) => (
               <Cell
@@ -139,14 +51,14 @@ export function PeriodBalanceChart() {
           <Legend
             verticalAlign="bottom"
             iconSize={24}
+            onMouseLeave={resetActiveIndex}
+            onMouseEnter={(_, index) => showSectorDetailsFor(index)}
             wrapperStyle={{
               paddingTop: 20,
             }}
             formatter={(value) => (
               <span className="font-bold capitalize">{value}</span>
             )}
-            onMouseLeave={() => setActiveIndex(undefined)}
-            onMouseEnter={(_, index) => setActiveIndex(index)}
           />
         </PieChart>
       </ResponsiveContainer>
