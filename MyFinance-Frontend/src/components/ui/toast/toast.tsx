2333b1eb534e771835dd8ac644211373215ee56c
Kevin Guedes <kevinguedes1@gmail.com>
2024-05-28 12:00:38 -0300

    fix: toast width

--- MyFinance-Frontend/src/components/ui/toast/toast.tsx
+++ MyFinance-Frontend/src/components/ui/toast/toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      'fixed left-1/2 top-0 z-[100] flex max-h-screen w-full -translate-x-1/2 flex-col-reverse p-4 sm:bottom-0 sm:left-auto sm:right-0 sm:top-auto sm:translate-x-0 sm:flex-col md:max-w-[420px]',
+      'fixed left-1/2 top-0 z-[100] flex max-h-screen w-full -translate-x-1/2 flex-col-reverse p-4 sm:bottom-0 sm:left-auto sm:right-0 sm:top-auto sm:max-w-[420px] sm:translate-x-0 sm:flex-col',
       className,
     )}
     {...props}
