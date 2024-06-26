7e12ba23bebd0722afc98597643dc22b71b2124a
Kevin Guedes <kevinguedes1@gmail.com>
2024-06-26 12:28:40 -0300

    fix: typying for custom transfers columns

--- MyFinance-Frontend/src/features/transfer/components/transfers-table/transfers-table-column.tsx
+++ MyFinance-Frontend/src/features/transfer/components/transfers-table/transfers-table-column.tsx
@@ -18,8 +18,9 @@ export const columns: ColumnDef<Transfer>[] = [
     accessorKey: 'value',
     header: 'Value',
     cell: ({ row }) => {
-      const value = row.getValue('value') as number
+      const value = row.getValue<number>('value')
       const formattedValue = toMoney(value, true)
+
       if (value > 0)
         return (
           <div className="min-w-20 text-left font-medium text-primary">
@@ -47,16 +48,15 @@ export const columns: ColumnDef<Transfer>[] = [
     accessorKey: 'settlementDate',
     header: 'Date',
     cell: ({ row }) => {
-      const payment = row.getValue('settlementDate') as Date
-      return (
-        <p>
-          {payment.toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: '2-digit',
-            year: 'numeric',
-          })}
-        </p>
-      )
+      const settlementDate = row
+        .getValue<Date>('settlementDate')
+        .toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: '2-digit',
+          year: 'numeric',
+        })
+
+      return <p>{settlementDate}</p>
     },
   },
   {
