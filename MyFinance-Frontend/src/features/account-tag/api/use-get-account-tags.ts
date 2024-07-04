ef555aee5ee667d56d1b321fbbdcd546eeead1cf
Kevin Guedes <kevinguedes1@gmail.com>
2024-07-04 13:31:13 -0300

    feat: infinite query for ats

--- MyFinance-Frontend/src/features/account-tag/api/use-get-account-tags.ts
+++ MyFinance-Frontend/src/features/account-tag/api/use-get-account-tags.ts
@@ -1,27 +1,38 @@
-import { keepPreviousData, useQuery } from '@tanstack/react-query'
+import { keepPreviousData, useInfiniteQuery } from '@tanstack/react-query'
 
 import { accountTagApi } from '../../common/api'
 import { Paginated } from '../../common/paginated'
 import { AccountTag } from '../models/account-tag'
 
-export const useGetAccountTags = (pageNumber: number, pageSize: number) => {
-  const query = useQuery({
-    queryKey: ['account-tags', { pageNumber, pageSize }],
+export const useGetAccountTags = (
+  managementUnitId: string,
+  pageSize: number,
+) => {
+  const infiniteQuery = useInfiniteQuery<Paginated<AccountTag>>({
+    queryKey: ['account-tags', { pageSize, managementUnitId }],
     staleTime: Infinity,
-    placeholderData: keepPreviousData,
-    queryFn: async () => {
+    retry: 3,
+    queryFn: async ({ pageParam }) => {
       const { data: paginatedAccountTags } = await accountTagApi.get<
         Paginated<AccountTag>
       >('', {
         params: {
-          pageNumber,
+          pageNumber: pageParam,
           pageSize,
+          managementUnitId,
         },
       })
 
       return paginatedAccountTags
     },
+    initialPageParam: 1,
+    getNextPageParam: (previousFetchedPage) =>
+      previousFetchedPage.hasNextPage
+        ? previousFetchedPage.pageNumber + 1
+        : undefined,
+    refetchOnWindowFocus: false,
+    placeholderData: keepPreviousData,
   })
 
-  return query
+  return infiniteQuery
 }
