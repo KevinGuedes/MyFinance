a76ce3877dbe765efcbaae666bd4e4628c3ec7da
Kevin Guedes <kevinguedes1@gmail.com>
2024-08-06 08:45:32 -0300

    refactor: transfer-item

--- MyFinance.UI/src/features/transfer/components/transfer-groups-list/transfer-item.tsx
+++ MyFinance.UI/src/features/transfer/components/transfer-groups-list/transfer-item.tsx
@@ -10,39 +10,38 @@ type TransferItemProps = {
 
 export function TransferItem({ transfer }: TransferItemProps) {
   return (
-    <li key={transfer.id} className="group flex flex-col gap-1 py-2">
-      <div className="flex items-center justify-between">
-        <p className="line-clamp-1 font-medium">{transfer.relatedTo}</p>
+    <li key={transfer.id} className="flex justify-between gap-2 py-2">
+      <div className="flex w-full flex-col gap-1">
+        <div className="flex items-center justify-between gap-2">
+          <p className="line-clamp-1 font-medium">{transfer.relatedTo}</p>
 
-        <div className="flex items-center gap-2">
           {transfer.type === TransferType.Income ? (
-            <p className="font-semibold text-primary">
+            <p className="shrink-0 font-semibold text-primary">
               {toMoney(transfer.value)}
             </p>
           ) : (
-            <p className="font-semibold text-destructive">
+            <p className="shrink-0 font-semibold text-destructive">
               {toMoney(-1 * transfer.value)}
             </p>
           )}
-          <TransferItemActions transfer={transfer} />
         </div>
-      </div>
 
-      <div className="flex flex-col justify-between gap-1">
         <div className="flex items-center justify-between gap-2">
           <p className="line-clamp-1 text-sm text-muted-foreground">
             {transfer.description}
           </p>
-          <div className="mr-8 flex shrink-0 gap-1">
-            <p className="rounded-md border px-1 py-px text-sm text-muted-foreground">
+          <div className="flex shrink-0 gap-2">
+            <p className="rounded-sm border px-1 text-sm text-muted-foreground">
               {transfer.tag}
             </p>
-            <p className="rounded-md border px-1 py-px text-sm text-muted-foreground">
+            <p className="rounded-sm border px-1 text-sm text-muted-foreground">
               {transfer.categoryName}
             </p>
           </div>
         </div>
       </div>
+
+      <TransferItemActions transfer={transfer} />
     </li>
   )
 }
